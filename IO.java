bc8af1e8c2f12782d6bc437f7bf5ba9c512c6056
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-22 17:36:58 +0530

    Writes Increment operation program

---
+++ IO.java
@@ -0,0 +1,18 @@
+class IO{
+public static void main(String[] args){
+System.out.println("Pre Increment operation...");
+int var1=100;
+int var2=++var1;
+int var3=++var2;
+System.out.println("var1:"+var1);
+System.out.println("var2:"+var2);
+System.out.println("var3:"+var3);
+System.out.println("Post Increment operation...");
+int num1=100;
+int num2=num1++;
+int num3=num2++;
+System.out.println("num1:"+num1);
+System.out.println("num2:"+num2);
+System.out.println("num3:"+num3);
+}
+}
