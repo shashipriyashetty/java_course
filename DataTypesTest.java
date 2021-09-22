e273cbfe2c2c6a889a866e97fe70da937125d915
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-22 10:42:18 +0530

    writes Datatype program

---
+++ DataTypesTest.java
@@ -0,0 +1,23 @@
+class DataTypesTest{
+public static void main(String[] args){
+byte a=10;
+System.out.println("a:"+a);
+int i=10000;
+System.out.println("i:"+i);
+long l=100000000;
+System.out.println("l:"+l);
+float f=10.00f;
+System.out.println("f:"+f);
+double d=20.120;
+System.out.println("d:"+d);
+char c='2';
+System.out.println("c:"+c);
+boolean b=true;
+System.out.println("b:"+b);
+}
+}
+
+
+
+
+
