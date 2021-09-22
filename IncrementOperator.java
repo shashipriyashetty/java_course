770b586d7e822563475c758e943ca47c3f30671a
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-22 11:02:03 +0530

    writes Increment Operator program

---
+++ IncrementOperator.java
@@ -0,0 +1,14 @@
+class IncrementOperator{
+public static void main(String[] args){
+System.out.println("pre Increment operation...");
+int a=1;
+int b=++a;
+System.out.println("a:"+a);
+System.out.println("b:"+b);
+System.out.println("post Increment operation...");
+int i=1;
+int j=i++;
+System.out.println("i:"+i);
+System.out.println("j:"+j);
+}
+}
