9587131732d8777c22ce5120f3fb10f4786eb192
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-22 10:53:41 +0530

    writes ArithmeticOperators program

---
+++ ArithmeticOperators.java
@@ -0,0 +1,24 @@
+class ArithmeticOperators{
+public static void main(String[] args){
+int a=10;
+int b=20;
+int addResult=a+b;
+System.out.println("addResult:"+addResult);
+int x=100;
+int y=50;
+int subResult=x-y;
+System.out.println("subResult:"+subResult);
+int p=5;
+int q=10;
+int mulResult=p*q;
+System.out.println("mulResult:"+mulResult);
+int i=5;
+int j=10;
+int divResult=j/i;;
+System.out.println("divResult:"+divResult);
+int m=9;
+int n=2;
+int modResult=m%n;
+System.out.println("modResult:"+modResult);
+}
+}
