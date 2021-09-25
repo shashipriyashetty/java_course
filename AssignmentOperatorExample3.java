031c79093267ae309cc332e77f77543753424324
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-25 17:41:06 +0530

    writes assignment operator example3 program

---
+++ AssignmentOperatorExample3.java
@@ -0,0 +1,12 @@
+class AssignmentOperatorExample3{
+public static void main(String args[]){
+int m=300;
+int n=200;
+m=m-n;
+System.out.println("m:"+m);
+int p=500;
+int q=100;
+p=p/q;
+System.out.println("p:"+p);
+}
+}
