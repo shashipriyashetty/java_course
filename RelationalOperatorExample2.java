0a2be12edcc0af9c6b2d1adfa97fd2cf6ba1064d
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-25 18:07:02 +0530

    writes relational operator example2 program

---
+++ RelationalOperatorExample2.java
@@ -0,0 +1,22 @@
+class RelationalOperatorExample2{
+public static void main(String[] args){
+int i=500;
+int j=200;
+int k=100;
+boolean result=i<j;
+System.out.println("i<j:"+result);
+boolean result1=j<i;
+System.out.println("j<i:"+result1);
+boolean result2=i>j;
+System.out.println("i>j:"+result2);
+boolean result3=j>i;
+System.out.println("j>i:"+result3);
+boolean result4=i<=k;
+System.out.println("i<=k:"+result4);
+boolean result5=i>=j;
+System.out.println("i>=j:"+result5);
+boolean result6=i==k;
+System.out.println("i==j:"+result6);
+
+}
+}
