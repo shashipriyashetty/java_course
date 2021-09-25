f363523ca91e46ab394ed87da0f2872b1ddd38a3
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-25 18:14:32 +0530

    writes logical operator example1 program

---
+++ LogicalOperatorExample1.java
@@ -0,0 +1,10 @@
+class LogicalOperatorExample1{
+public static void main(String args[]){
+int a=200;
+int b=100;
+System.out.println(a>b&&a==b);
+System.out.println(a>=b&&a<=b);
+System.out.println(a<b||a==b);
+System.out.println(a==b||a<b);
+}
+}
