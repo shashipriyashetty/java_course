e1480b5efc03830f9d7a14bf029b3d992c297722
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-25 17:40:40 +0530

    writes assignment operator example2 program

---
+++ AssignmentOperatorExample2.java
@@ -0,0 +1,12 @@
+class AssignmentOperatorExample2{
+public static void main(String args[]){
+int m=100;
+int n=200;
+m=m+n;
+System.out.println("m:"+m);
+int p=5;
+int q=150;
+p=p*q;
+System.out.println("p:"+p);
+}
+}
