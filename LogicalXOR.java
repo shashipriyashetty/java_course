6bbfc07c4f963fb3eb1f3e0e405feec67cf31ac4
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-27 19:13:48 +0530

    Writes Logical XOR operator program

---
+++ LogicalXOR.java
@@ -0,0 +1,10 @@
+class LogicalXOR{
+public static void main(String[] args){
+System.out.println((5==5)^(5==6));
+System.out.println((5==5)^(6==6));
+System.out.println((4==5)^(5==6));
+System.out.println((5==5)^(6==6));
+System.out.println((5==5)^(5==6)^(6==7)^(7==8));
+
+}
+}
