d7f98e628d0d4f13ce9d4e4af20eeec8a486eeb1
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-27 18:47:55 +0530

    Writes Nested Ternary operator program

---
+++ NestedTernaryOperator.java
@@ -0,0 +1,8 @@
+class NestedTernaryOperator{
+public static void main(String[] args){
+int mark=65;
+char result;
+result=mark>=80?'A':mark>=60 && mark<80 ? 'B' :'C';
+System.out.println(result);
+}
+}
