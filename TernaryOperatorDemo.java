f1906c841d38a9aa9526a037f90f78dbb4837544
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-27 17:26:25 +0530

    Writes Ternary operator program

---
+++ TernaryOperatorDemo.java
@@ -0,0 +1,10 @@
+class TernaryOperatorDemo{
+public static void main(String[] args){
+int x=100;
+int y=200;
+int result=x>y?x:y;
+System.out.println("largest of" +x+ "&" +y+ "is:"+result);
+int result1=x<y?x:y;
+System.out.println("Smallest of" +x+ "&" +y+ "is:"+result1);
+}
+}
