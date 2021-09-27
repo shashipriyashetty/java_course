adb56f236d379517090b6bc9709854d9e90f6f40
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-27 19:01:24 +0530

    Writes Shift operator program

---
+++ ShiftOperator.java
@@ -0,0 +1,12 @@
+class ShiftOperator{
+public static void main(String[] args){
+int a=3;
+int b=2;
+int c=1;
+System.out.println(a<<1);
+System.out.println(b>>1);
+System.out.println(c>>>1);
+}
+}
+
+
