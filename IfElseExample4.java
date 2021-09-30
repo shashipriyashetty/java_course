3edf095878ad130fe53de1f00cf44fb1a2ead8ae
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-30 14:56:19 +0530

    Writes If Else program example4

---
+++ IfElseExample4.java
@@ -0,0 +1,15 @@
+class IfElseExample4{
+static int a=99;
+static int b=88888;
+public static void main(String[] args){
+if(a<100){
+System.out.println("Two digit Number");
+}
+if(b>100){
+System.out.println("More than two digit number");
+}
+else{
+System.out.println("Two digit number");
+}
+}
+}
