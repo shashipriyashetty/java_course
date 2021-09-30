f97fc7f8a6ce09d82d8d0da622db9e40830ce64f
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-30 14:55:58 +0530

    Writes If Else program example3

---
+++ IfElseExample3.java
@@ -0,0 +1,11 @@
+class IfElseExample3{
+static long n=1000000000;
+public static void main(String[] args){
+if(n>1000000000){
+System.out.println("Valid Number");
+}
+else{
+System.out.println("Invalid Number");
+}
+}
+}
