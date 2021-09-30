49bdbc9f693804873bda80a71e9eff86bae41ff8
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-30 14:55:16 +0530

    Writes If Else program example1

---
+++ IfElseExample1.java
@@ -0,0 +1,15 @@
+class IfElseExample1{
+public static void main(String[] args){
+int n=76;
+int m=97;
+if(n%2==0){
+System.out.println("Even Number");
+}
+if(m%2==0){
+System.out.println("Even Number");
+}
+else{
+System.out.println("Odd Number");
+}
+}
+}
