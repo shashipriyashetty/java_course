3efae0f5668ab1afa9cc743f619444ba683292fd
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-09-30 14:55:42 +0530

    Writes If Else program example2

---
+++ IfElseExample2.java
@@ -0,0 +1,12 @@
+class IfElseExample2{
+public static void main(String args[]){
+boolean a=true;
+boolean b=false;
+if(a){
+System.out.println("Hello Developers");
+}
+else{
+System.out.println("Hello JOkers");
+}
+}
+}
