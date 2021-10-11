ffb698fd32b8688ffd219575f8fcb56a2869a871
shashipriyashetty <shashipriyabshetty@gmail.com>
2021-10-11 07:06:56 +0530

    Writes IfElseIfLadder program

---
+++ IfElseIfLadderDemo.java
@@ -0,0 +1,19 @@
+class IfElseIfLadderDemo{
+public static void main(String[] args){
+boolean aadhar=true;
+boolean panCard=false;
+boolean voterId=false;
+if(voterId){
+System.out.println("You are a Indian Citizen");
+}
+else if(panCard){
+System.out.println("You are a indian citizen");
+}
+else if(aadhar){
+System.out.println("You are a indian citizen");
+}
+else{
+System.out.println("You are not a indian citizen");
+}
+}
+}
